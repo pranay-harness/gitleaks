fc271feb8179225e00038658bd414ea78c025eea
Pranay Shah <89774391+pranay-harness@users.noreply.github.com>
2023-01-05 08:02:12 +0530

    Update repolist.sh

--- git-repos/repolist.sh
+++ git-repos/repolist.sh
@@ -1,4 +1,3 @@
-git clone https://github.com/harness/harness-core
 git clone https://github.com/harness/harness-core-ui
 git clone https://github.com/harness/developer-hub
 git clone https://github.com/harness/lite-engine
